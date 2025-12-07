a0f45e5f9cdde5e5d19dd015bbb0ab0efdba31b1
Suvro Biswas <shuvo.249963@gmail.com>
2025-12-08 02:18:58 +0600

    Add router.jsx and add router provider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from "react-router/dom";
+import PrivateRoute from './Provider/PrivateRoute.jsx';
+import { ToastContainer } from 'react-toastify';
+import { router } from './Router/Router.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PrivateRoute>
+       <RouterProvider router={router} />
+    </PrivateRoute>
+    <ToastContainer></ToastContainer>
   </StrictMode>,
 )
