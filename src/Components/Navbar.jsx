e2ea0c3e3b026162a4988b2e263112241ce37b67
Suvro Biswas <shuvo.249963@gmail.com>
2025-12-08 23:16:11 +0600

    add home.jsx and do routes setup

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
     }
     return (
         <div className="navbar bg-base-100 shadow-sm fixed top-0 left-0 w-full z-50  ">
-            <div className="container  mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+            <div className="container  mx-auto  sm:px-6 lg:px-8 flex justify-between items-center">
                 {/* Left section (Logo + Dropdown) */}
                 <div className="flex items-center gap-1 shrink-0 ">
                     {/* Mobile dropdown */}
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router";
 import HomeLayout from "../Layouts/HomeLayout";
+import Home from "../Pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout></HomeLayout>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/all-tickets',
+      }
+    ]
   },
 ]);
