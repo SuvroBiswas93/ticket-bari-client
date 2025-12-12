11935d782b792743a7cb0736ef39bbbdd863b5d2
Suvro Biswas <shuvo.249963@gmail.com>
2025-12-12 23:35:57 +0600

    Add use role hook to toggle the role of user

---
+++ src/hooks/useRole.jsx
@@ -0,0 +1,37 @@
+// useRole.jsx
+import { use, useEffect, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import useAxiosSecure from "./useAxiosSecure";
+import { toast } from "react-toastify";
+
+const useRole = () => {
+  const { user, loading } = use(AuthContext);
+  const axiosSecure = useAxiosSecure()
+
+  const [role, setRole] = useState(null);
+  const [isRoleLoading, setIsRoleLoading] = useState(true);
+
+  useEffect(() => {
+    if (loading) return;           
+    if (!user?.email) return;      
+
+    const fetchRole = async () => {
+      setIsRoleLoading(true);
+      try {
+        const result = await axiosSecure(`/user/role?email=${user.email}`);
+        setRole(result.data.role);
+      } catch (error) {
+        toast.error(error?.response?.data?.message || error.message || "Failed to fetch role")
+        setRole(null);
+      } finally {
+        setIsRoleLoading(false);
+      }
+    };
+
+    fetchRole();
+  }, [user, loading, axiosSecure]);
+
+  return [role, isRoleLoading];
+};
+
+export default useRole;
