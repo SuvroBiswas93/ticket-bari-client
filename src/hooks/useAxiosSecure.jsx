c4f454296bc7cd8bafe9f8159be13fdb11a30ba0
Suvro Biswas <shuvo.249963@gmail.com>
2025-12-13 17:35:28 +0600

    implement utils in index.j and export save or updateuser and image upload using imagebb with that secret the credentials

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,8 @@ import axios from 'axios';
 import { AuthContext } from '../Provider/AuthProvider';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials:true
 });
 
 const useAxiosSecure = () => {
