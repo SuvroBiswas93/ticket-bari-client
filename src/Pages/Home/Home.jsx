03d49432cf99fee4908f0071fc1e97725978e2a9
Suvro Biswas <shuvo.249963@gmail.com>
2025-12-16 01:56:51 +0600

    Add two section in Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import HeroSlider from '../HeroSlider/HeroSlider';
 import PopularRoutes from '../PopularRoutes/PolpularRoutes';
+import AdvertisementSection from './AdvertisementSection/AdvertisementSection';
+import LatestTicketsSection from './LatestTicketsSection/LatestTicketsSection';
 
 const Home = () => {
     return (
         <div>
             <HeroSlider></HeroSlider>
+            <AdvertisementSection></AdvertisementSection>
+            <LatestTicketsSection></LatestTicketsSection>
             <PopularRoutes></PopularRoutes>
         </div>
     );
